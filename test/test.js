e482b7e1b0a036c1589203611b8b6757dc253b05
Smilla Sollén <ss226uk@student.lnu.se>
2025-09-22 14:09:52 +0200

    Chaneg name from generic colorutils to WcagColorService

--- test/test.js
+++ test/test.js
@@ -5,18 +5,18 @@
  * @version 1.0.0
  */
 
-import { ColorUtils } from '../src/utils/colorUtils.js'
+import { WcagColorService } from '../src/utils/WcagColorService.js'
 import { WcagCheck } from '../src/utils/WcagCheck.js'
 
-const colorUtils = new ColorUtils()
+const wcagService = new WcagColorService()
 
 console.log('Testing contrastRatio...')
-console.log(colorUtils.contrastRatio('#FFFFFF', '#000000')) // Expected: 21
-console.log(colorUtils.contrastRatio('#FF0000', '#00FF00')) // Expected: ~2.91
+console.log(wcagService.contrastRatio('#FFFFFF', '#000000')) // Expected: 21
+console.log(wcagService.contrastRatio('#FF0000', '#00FF00')) // Expected: ~2.91
 
 console.log('Testing passesWcag...')
 const check1 = new WcagCheck('#FFFFFF', '#000000', { level: 'AAA' })
-console.log(colorUtils.passesWcag(check1)) // Expected: true
+console.log(wcagService.passesWcag(check1)) // Expected: true
 
 const check2 = new WcagCheck('#FF0000', '#00FF00', { isLargeText: true})
-console.log(colorUtils.passesWcag(check2)) // Expected: false
+console.log(wcagService.passesWcag(check2)) // Expected: false
