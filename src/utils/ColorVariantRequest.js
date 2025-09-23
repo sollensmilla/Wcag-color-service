db72f582ff1809d254688089ae6179b173acbb0d
Smilla Sollén <ss226uk@student.lnu.se>
2025-09-23 16:56:21 +0200

    Finnish up the request class

--- src/utils/ColorVariantRequest.js
+++ src/utils/ColorVariantRequest.js
@@ -8,7 +8,7 @@
 /**
  *
  */
-class ColorVariantRequest {
+export class ColorVariantRequest {
   /**
    * Creates an instance of ColorVariantRequest.
    *
@@ -20,5 +20,49 @@ class ColorVariantRequest {
     this.level = 'AA'
     this.isLargeText = false
     this.direction = 'lighten'
+  }
+
+  /**
+   * Sets the background color for the color variant request.
+   *
+   * @param {string} background - The background color in hex format (e.g., '#RRGGBB').
+   * @returns {ColorVariantRequest} The updated ColorVariantRequest instance.
+   */
+  withBackground (background) {
+    this.background = background
+    return this
+  }
+
+  /**
+   * Sets the WCAG level for the color variant request.
+   *
+   * @param {string} level - The WCAG level to check against ('AA' or 'AAA').
+   * @returns {ColorVariantRequest} The updated ColorVariantRequest instance.
+   */
+  withLevel (level) {
+    this.level = level
+    return this
+  }
+
+  /**
+   * Sets whether the text is considered large for the color variant request.
+   *
+   * @param {boolean} isLargeText - True if the text is large, false otherwise.
+   * @returns {ColorVariantRequest} The updated ColorVariantRequest instance.
+   */
+  withLargeText (isLargeText) {
+    this.isLargeText = isLargeText
+    return this
+  }
+
+  /**
+   * Sets the direction for the color variant request.
+   *
+   * @param {string} direction - The direction for the color variant ('lighten' or 'darken').
+   * @returns {ColorVariantRequest} The updated ColorVariantRequest instance.
+   */
+  withDirection (direction) {
+    this.direction = direction
+    return this
   }
 }
--- src/utils/WcagCheck.js
+++ src/utils/WcagCheck.js
@@ -13,8 +13,10 @@ export class WcagCheck {
    *
    * @param foreground - The foreground color in hex format.
    * @param background - The background color in hex format.
+   * @param level.level
    * @param level - The WCAG level to check against ('AA' or 'AAA').
    * @param isLargeText - Whether the text is considered large (18pt or 14pt bold).
+   * @param level.isLargeText
    */
   constructor (foreground, background, { level = 'AA', isLargeText = false } = {}) {
     this.foreground = foreground
