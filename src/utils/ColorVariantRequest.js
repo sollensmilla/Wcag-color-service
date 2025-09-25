4aebc8fc6762b0a3adc35baaa4976ca62e977081
Smilla Sollén <ss226uk@student.lnu.se>
2025-09-25 15:01:05 +0200

    Refine docs

--- src/utils/ColorVariantRequest.js
+++ src/utils/ColorVariantRequest.js
@@ -6,14 +6,9 @@
  */
 
 /**
- * Class representing a color variant request.
+ * Sets the base color and preferences for generating accessible color variants.
  */
 export default class ColorVariantRequest {
-  /**
-   * Creates an instance of ColorVariantRequest.
-   *
-   * @param {string} basecolor - The base color in hex format (e.g., '#RRGGBB').
-   */
   constructor(basecolor) {
     this.basecolor = basecolor
     this.level = 'AA'
@@ -21,34 +16,16 @@ export default class ColorVariantRequest {
     this.direction = 'lighten'
   }
 
-  /**
-   * Sets the WCAG level for the color variant request.
-   *
-   * @param {string} level - The WCAG level to check against ('AA' or 'AAA').
-   * @returns {ColorVariantRequest} The updated ColorVariantRequest instance.
-   */
   withLevel(level) {
     this.level = level
     return this
   }
 
-  /**
-   * Sets whether the text is considered large for the color variant request.
-   *
-   * @param {boolean} isLargeText - True if the text is large, false otherwise.
-   * @returns {ColorVariantRequest} The updated ColorVariantRequest instance.
-   */
   withLargeText(isLargeText) {
     this.isLargeText = isLargeText
     return this
   }
 
-  /**
-   * Sets the direction for the color variant request.
-   *
-   * @param {string} direction - The direction for the color variant ('lighten' or 'darken').
-   * @returns {ColorVariantRequest} The updated ColorVariantRequest instance.
-   */
   withDirection(direction) {
     this.direction = direction
     return this
